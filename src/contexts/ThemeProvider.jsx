db34e260b34a2c62109edd87d7d21bf513f71d29
Kalpak43 <111725804+Kalpak43@users.noreply.github.com>
2024-05-05 20:52:32 +0530

    UPDATE: Darkmode using next-themes

--- src/contexts/ThemeProvider.jsx
+++ src/contexts/ThemeProvider.jsx
@@ -1,48 +1,15 @@
 "use client";
-import React, { createContext, useEffect, useState } from "react";
 
-export const ThemeContext = createContext();
+import React from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState("light");
-
-  useEffect(() => {
-    const localTheme = localStorage.getItem("kp-theme");
-
-    if (localTheme) {
-      setTheme(localTheme);
-      document.body.setAttribute("data-theme", localTheme);
-    } else {
-      const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
-      document.body.setAttribute(
-        "data-theme",
-        {
-          true: "dark",
-          false: "light",
-        }[prefersDarkMode.matches]
-      );
-
-      setTheme(prefersDarkMode.matches ? "dark" : "light");
-    }
-  }, []);
-
-  const toggleTheme = () => {
-    setTheme((prevTheme) => {
-      if (prevTheme === "light") {
-        document.body.setAttribute("data-theme", "dark");
-        localStorage.setItem("kp-theme", "dark");
-        return "dark";
-      } else {
-        document.body.setAttribute("data-theme", "light");
-        localStorage.setItem("kp-theme", "light");
-        return "light";
-      }
-    });
-  };
-
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
   );
 }
