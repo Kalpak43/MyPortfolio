5035205961883387fe2aa32195a7f1ab91cbdacb
Kalpak Goshikwar <111725804+Kalpak43@users.noreply.github.com>
2024-05-07 18:31:08 +0530

    Update Skills.jsx

--- src/components/LandingPage/Skills.jsx
+++ src/components/LandingPage/Skills.jsx
@@ -7,12 +7,12 @@ export default function Skills() {
       <h1 className=" text-[var(--md-sys-color-primary)]">Skills</h1>
       <br />
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        {skills.map((skill) => (
-          <div className="rounded-3xl bg-[var(--md-sys-color-surface-container)] p-4">
+        {skills.map((skill, index) => (
+          <div key={index}  className="rounded-3xl bg-[var(--md-sys-color-surface-container)] p-4">
             <h2>{skill.title}</h2>
             <div className="">
-              {skill.skills.map((skill) => (
-                <div className="w-fit p-1 px-4 rounded-lg bg-[var(--md-sys-color-primary-container)] text-[var(--md-sys-color-primary)] float-left m-2">
+              {skill.skills.map((skill, index) => (
+                <div key={100+index} className="w-fit p-1 px-4 rounded-lg bg-[var(--md-sys-color-primary-container)] text-[var(--md-sys-color-primary)] float-left m-2">
                   <h4 className=" font-[600]">{skill[0]}</h4>
                 </div>
               ))}
